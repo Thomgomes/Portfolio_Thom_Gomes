ef3b1349ed8d73781dc9e1af3f9c3f295cdc8596
ThomGomes <math.011@hotmail.com>
2023-02-15 19:18:45 -0300

    add themeBtn dropdown

--- src/components/navbar/navLink.jsx
+++ src/components/navbar/navLink.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export const NavLinks = (props) => {
   return (
     <li>
-      <Link to={props.path} className="text-sm hover:underline">
+      <Link to={props.path} className="text-sm pt-3 transition-all duration-75 hover:border-t-2 hover:shadow-sm">
         {props.nameDescription}
       </Link>
     </li>
