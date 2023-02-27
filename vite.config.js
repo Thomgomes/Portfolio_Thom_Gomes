a97afae5b159e63ccdd8484a1c331fb616673d36
ThomGomes <math.011@hotmail.com>
2023-02-27 13:11:51 -0300

    att vite-base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://thomgomes.github.io/Portfloio_Thom_Gomes/",
+  base: "thomgomes.github.io/Portfloio_Thom_Gomes/",
   plugins: [react()],
 })
