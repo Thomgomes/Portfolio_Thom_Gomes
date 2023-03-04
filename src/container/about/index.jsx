cf31d8bc3c821a424e9fcc2828d55df9067684ae
ThomGomes <math.011@hotmail.com>
2023-03-04 14:35:59 -0300

    margin about alterade

--- src/container/about/index.jsx
+++ src/container/about/index.jsx
@@ -22,7 +22,7 @@ export const About = () => {
 
   return (
     <section className="section lg:mb-48 xl:mb-0" id="about">
-      <div className="container mx-auto my-auto">
+      <div className="container mx-auto mb-auto">
         <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           <AboutImg />
           <AboutTexts
