c88c44e16b709f519464eb44e9aba6ac0e9a681c
ThomGomes <math.011@hotmail.com>
2023-03-07 19:55:52 -0300

    title alterate

--- src/container/ProjectsPage/index.jsx
+++ src/container/ProjectsPage/index.jsx
@@ -17,18 +17,21 @@ export const AllProjects = () => {
             <section className="mb-10">
               {/* titulo */}
               <div
-                className="sm:mx-16 mb-10 min-w-min mx-auto bg-white rounded-bl-md rounded-tl-md relative
-                         before:hidden md:before:block before:absolute before:right-0 before:h-full before:w-24 before:bg-[#250821] before:rounded-l-[50%]
+                className="sm:mx-16 mb-10 min-w-min mx-auto rounded-bl-md rounded-tl-md relative
+
               ">
                 <div className="flex justify-center md:justify-start">
                   <h2 className="pl-4 p-2 text-5xl font-semibold text-[#bb700f]">Projetos</h2>
                 </div>
 
+
+                {/* um teste qie não deu mt certo */}
+                {/* before:hidden md:before:block before:absolute before:right-0 before:h-full before:w-24 before:bg-[#250821] before:rounded-l-[50%]
                 <div className="absolute bg-[#250821] w-16 h-3 top-0 right-0 md:left-2/3 rounded-bl-full rounded-br-full"></div>
                 <div className="absolute bg-[#250821] w-16 h-3 bottom-0 right-0 md:left-2/3 rounded-tl-full rounded-tr-full"></div>
 
                 <div className="absolute bg-[#250821] w-16 h-3 top-0 left-0 rounded-bl-full rounded-br-full md:hidden"></div>
-                <div className="absolute bg-[#250821] w-16 h-3 bottom-0 left-0 rounded-tl-full rounded-tr-full md:hidden"></div>
+                <div className="absolute bg-[#250821] w-16 h-3 bottom-0 left-0 rounded-tl-full rounded-tr-full md:hidden"></div> */}
               </div>
 
               {/* Slider dos projetos */}
