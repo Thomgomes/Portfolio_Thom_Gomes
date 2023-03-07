e17bc433f5a0fc9d4dee0a51801aee4337da503e
ThomGomes <math.011@hotmail.com>
2023-03-07 20:34:05 -0300

    animation in title porjects section

--- src/components/projects/projectTitle.jsx
+++ src/components/projects/projectTitle.jsx
@@ -1,14 +1,23 @@
+import { motion } from 'framer-motion'
+
+import { fadein } from '../../utils/variants'
+
 import { WorkLink } from "./linkWorks"
 
 export const ProjectTitle = () => {
   return (
-    <div className="text-center">
+    <motion.div
+      variants={fadein('', 0.3)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: true, amount: 0.3 }}
+      className="text-center">
       <h2 className="leading-tight text-5xl font-semibold mb-6 tracking-[10%] md:mx-0 mx-3">
         Projetos em <br />
         Destaque
       </h2>
 
       <WorkLink id="/projects" />
-    </div>
+    </motion.div>
   )
 }
